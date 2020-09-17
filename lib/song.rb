2143e1fc52dc76a9fc03cc67e490ffe36a1c9efc
Brock Byrd <brockbyrdd@gmail.com>
2020-09-17 22:21:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,10 +19,9 @@ class Song
   end
 
   def self.new_by_name(name)
-    @name = n
-    binding.pry
-    name = self.new
-    name
+    song = self.new
+    song.name = name
+    song
   end
 
   def self.create_by_name(name)
