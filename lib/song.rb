50df1501df8cc2ef8addec905866fca9a709f061
Brock Byrd <brockbyrdd@gmail.com>
2020-09-17 21:59:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,10 @@ class Song
     self.class.all << self
   end
 
-  def self.create
+  def self.create(song)
     song = self.new
+    song.name = name
+    @@all << song
   end
 
   def self.new_by_name(name)
